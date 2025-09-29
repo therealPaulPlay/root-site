b8464e0388458a0cb0730b1d05114afc40f276c8
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2025-09-29 19:06:05 +0200

    chore: project setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href="/images/favicon.svg" />
+</svelte:head>
+
+{@render children?.()}
---
+++ svelte.config.js
@@ -0,0 +1,4 @@
+import adapter from '@sveltejs/adapter-static';
+const config = { kit: { adapter: adapter() } };
+
+export default config;
