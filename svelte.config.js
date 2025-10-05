7a47f1ebdfea76d5dec8df5f3b9034ecdd6e2b3d
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2025-10-05 02:11:22 +0200

    chore: fix build setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
 import adapter from '@sveltejs/adapter-static';
-const config = { kit: { adapter: adapter() } };
+const config = { kit: { adapter: adapter({ fallback: 'index.html' }) } }; // index.html fallback for SPA
 
 export default config;
