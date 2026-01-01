884f7743ca623bbaf80d2103c0c481ab3146696d
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2026-01-01 01:32:37 +0100

    style: fix error message height

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -6,7 +6,7 @@
 	<title>Error</title>
 </svelte:head>
 
-<div class="flex h-full w-full flex-col items-center justify-center">
+<div class="flex h-screen w-full flex-col items-center justify-center">
 	<h1>{page.status} ({page.error.message}).</h1>
 	<p>Beep. Bop.</p>
 </div>
