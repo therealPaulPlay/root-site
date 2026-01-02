b72cc9006308e136905839544f8c768a2f92d7c2
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2026-01-02 21:06:22 +0100

    feat: add subtle vibration to alert dialog action button

--- src/lib/components/ui/alert-dialog/alert-dialog-action.svelte
+++ src/lib/components/ui/alert-dialog/alert-dialog-action.svelte
@@ -2,13 +2,18 @@
 	import { AlertDialog as AlertDialogPrimitive } from "bits-ui";
 	import { buttonVariants } from "$lib/components/ui/button/index.js";
 	import { cn } from "$lib/utils.js";
+	import { vibrate } from "$lib/utils/haptics";
 
-	let { ref = $bindable(null), class: className, ...restProps } = $props();
+	let { ref = $bindable(null), class: className, onclick: execFunc, ...restProps } = $props();
 </script>
 
 <AlertDialogPrimitive.Action
 	bind:ref
 	data-slot="alert-dialog-action"
 	class={cn(buttonVariants(), className)}
+	onclick={() => {
+		vibrate.light();
+		execFunc?.();
+	}}
 	{...restProps}
 />
