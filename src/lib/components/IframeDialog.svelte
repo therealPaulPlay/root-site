34562c95807766739c8eb565ca9b6092d2421652
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2026-01-09 16:12:07 +0100

    style: iframe dialog height adjustment

--- src/lib/components/IframeDialog.svelte
+++ src/lib/components/IframeDialog.svelte
@@ -8,7 +8,7 @@
 	<Dialog.Trigger class={variant ? buttonVariants({ variant: variant }) : "" + classes ? " " + classes : ""}>
 		{@render children?.()}
 	</Dialog.Trigger>
-	<Dialog.Content class="flex h-[calc(100svh-10rem)] flex-col overflow-hidden pt-12">
+	<Dialog.Content class="flex h-[calc(100svh-7.5rem)] flex-col overflow-hidden pt-12">
 		<iframe frameborder="0" class="w-full grow border" src={src + "?hide-navbar=true&hide-footer=true"} title="web page"> </iframe>
 	</Dialog.Content>
 </Dialog.Root>
