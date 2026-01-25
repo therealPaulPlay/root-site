696e83e06272914b7be807b9d933ce0abba1e6f9
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2026-01-25 22:53:35 +0100

    chore: adjust title

--- src/routes/blog/building-your-own-security-camera/+page.svelte
+++ src/routes/blog/building-your-own-security-camera/+page.svelte
@@ -11,7 +11,7 @@
 </script>
 
 <svelte:head>
-	<title>How to build your own privacy-preserving home security camera (Raspberry Pi Zero 2)</title>
+	<title>Building your own private security camera (Raspberry Pi Zero 2)</title>
 </svelte:head>
 
 <ContentHero
