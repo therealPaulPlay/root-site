7bff21c010d17686466b2405a29d6b2ee5f06dfe
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2026-01-26 19:50:32 +0100

    style: revert border decision, smoothly fade in iframe dialog content

--- src/lib/components/IframeDialog.svelte
+++ src/lib/components/IframeDialog.svelte
@@ -1,7 +1,10 @@
 <script>
 	import * as Dialog from "$lib/components/ui/dialog";
 	import { buttonVariants } from "./ui/button";
+
 	let { children, src = "/", variant = "outline", class: classes } = $props();
+
+	let iframeLoaded = $state(false);
 </script>
 
 <Dialog.Root>
@@ -9,12 +12,21 @@
 		{@render children?.()}
 	</Dialog.Trigger>
 	<Dialog.Content class="flex h-[calc(100svh-7.5rem)] flex-col overflow-hidden pt-12">
-		<iframe
-			frameborder="0"
-			class="w-full grow border"
-			src={src + "?hide-navbar=true&hide-footer=true"}
-			title="web page"
-		>
-		</iframe>
+		<div class="relative w-full grow border">
+			<div
+				class="pointer-events-none absolute inset-0 h-full w-full bg-neutral-300 transition"
+				class:opacity-0={iframeLoaded}
+				class:animate-pulse={!iframeLoaded}
+			></div>
+			<iframe
+				frameborder="0"
+				class:opacity-0={!iframeLoaded}
+				onload={() => (iframeLoaded = true)}
+				class="h-full w-full transition"
+				src={src + "?hide-navbar=true&hide-footer=true"}
+				title="web page"
+			>
+			</iframe>
+		</div>
 	</Dialog.Content>
 </Dialog.Root>
