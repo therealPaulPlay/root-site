d409ddcebbe4c19b662ebf131253df3b6af8dd5a
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2026-04-16 21:47:56 +0200

    style: adjust gap for audio detection label

--- src/lib/components/CameraDetectionDialog.svelte
+++ src/lib/components/CameraDetectionDialog.svelte
@@ -68,7 +68,7 @@
 							</div>
 						{/each}
 					{:else if event.audioDetection}
-						<div class="absolute inset-0 flex flex-col items-center justify-center gap-3 bg-foreground/50 dark:bg-background/50">
+						<div class="absolute inset-0 flex flex-col items-center justify-center gap-2 bg-foreground/50 dark:bg-background/50">
 							<RiVolumeUpLine class="size-8 text-background dark:text-foreground" />
 							<Label class="text-background dark:text-foreground">{event.audioDetection.label}</Label>
 						</div>
