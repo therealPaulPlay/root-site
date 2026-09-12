31c4f84f181242cef104d7f3142c6c45206faba1
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2026-09-12 20:04:00 +0200

    fix: vite svelte config shape, bump svelte dependency

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit({ kit: { adapter: adapter({ fallback: "index.html" }) } })]
+	plugins: [tailwindcss(), sveltekit({ adapter: adapter({ fallback: "index.html" }) })]
 });
